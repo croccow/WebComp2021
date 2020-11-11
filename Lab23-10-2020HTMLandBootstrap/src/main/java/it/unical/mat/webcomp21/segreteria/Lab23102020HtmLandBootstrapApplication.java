19242320569e335ffa02734f4b60d15ad79ff2f8
Walter <crcwtr96m28086r@studenti.unical.it>
2020-11-11 23:41:30 +0100

    first commit web

---
+++ Lab23-10-2020HTMLandBootstrap/src/main/java/it/unical/mat/webcomp21/segreteria/Lab23102020HtmLandBootstrapApplication.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.segreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab23102020HtmLandBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab23102020HtmLandBootstrapApplication.class, args);
+	}
+
+}
---
+++ Lab23-10-2020HTMLandBootstrap/src/test/java/it/unical/mat/webcomp21/segreteria/Lab23102020HtmLandBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.segreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab23102020HtmLandBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
